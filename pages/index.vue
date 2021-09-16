95f21125a59c1952742760159004d1b292492c8f
Domenico Di Sarno <domenico.disarno@abstract.it>
2021-09-16 10:18:57 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      E-Commerce POC with NuxtJs + TS
+    </h1>
+    <ul>
+      <li>
+        <nuxt-link to="/products">
+          Products
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
---
+++ pages/products/_upc/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Product {{ $route.params.upc }}</h1>
+</template>
